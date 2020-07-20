60bf5ec28002b8d4bc6c11356126d3dc46e32ad6
ujjawal123@ <UJJAWAL AYUSH@DESKTOP-5QIP920>
2020-07-20 23:37:57 +0530

    Added Home Screen Facilities

--- app/src/main/java/com/ujjawalayush/example/kamsh/Fragments/ContestFragment.java
+++ app/src/main/java/com/ujjawalayush/example/kamsh/Fragments/ContestFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +16,15 @@ public class ContestFragment extends Fragment {
         // Required empty public constructor
     }
     View home;
+    RecyclerView my,future,past,active;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         home =inflater.inflate(R.layout.fragment_contest, container, false);
+        my=(RecyclerView)home.findViewById(R.id.recyclerView);
+        active=(RecyclerView)home.findViewById(R.id.recyclerView1);
+        future=(RecyclerView)home.findViewById(R.id.recyclerView2);
+        past=(RecyclerView)home.findViewById(R.id.recyclerView3);
         return home;
     }
 }
